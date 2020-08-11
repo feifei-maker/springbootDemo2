c6401cfdc953d442b8f28db14e0d51e438df92a4
feifei-maker <whc_feifei@163.com>
2020-08-11 19:22:31 +0800

    Initial commit

---
+++ src/main/java/com/whc/springboot/config/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package com.whc.springboot.config;
+
+/**
+ * ClassName: WebMvcConfig <br/>
+ * Description: <br/>
+ * date: 2020/8/11 18:35<br/>
+ *
+ * @author FEI FEI<br/>
+ * @since JDK 1.8
+ */
+public class WebMvcConfig {
+
+}
---
+++ src/main/java/com/whc/springboot/modules/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.whc.springboot.modules.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * ClassName: TestController <br/>
+ * Description: <br/>
+ * date: 2020/8/11 18:37<br/>
+ *
+ * @author FEI FEI<br/>
+ * @since JDK 1.8
+ */
+@Controller
+@RequestMapping("/testController")
+public class TestController {
+    @GetMapping("/testMethod")
+    @ResponseBody
+    public String testMethod(){
+        return "This is a testMethod";
+    }
+}
